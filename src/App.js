9df43650b434d3b092f0f47dc897b5785842a503
IsacHolstad <wisac22@gmail.com>
2023-04-19 19:33:54 +0200

    loader made

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './index.css';
 import Router from "./routes/Router";
+import {useSelector} from "react-redux";
 import HeaderNavigation from "./components/shared/HeaderNavigation";
 import Footer from "./components/shared/Footer";
+import Loader from "./components/shared/Loader";
 function App() {
+    //const {isLoading} = useSelector(state => state.loader)
   return (
     <>
       <HeaderNavigation/>
       <Router/>
+      <Loader/>
       <Footer/>
     </>
 
