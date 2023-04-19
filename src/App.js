fbdadf39491fa2432ab2a7eb7f226f417db23188
IsacHolstad <wisac22@gmail.com>
2023-04-19 13:18:07 +0200

    header navigation done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './index.css';
 import Router from "./routes/Router";
+import HeaderNavigation from "./components/shared/HeaderNavigation";
 function App() {
   return (
     <>
+      <HeaderNavigation/>
       <Router/>
     </>
 
