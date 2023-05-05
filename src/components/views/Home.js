584bd1b3ef228f668e042fdc3129b924d48126b9
IsacHolstad <wisac22@gmail.com>
2023-05-05 14:12:52 +0200

    using usememo for venues to load faster

--- src/components/views/Home.js
+++ src/components/views/Home.js
@@ -1,6 +1,6 @@
 import {useDispatch, useSelector} from "react-redux";
 import {Link} from "react-router-dom";
-import React, {useEffect} from "react";
+import React, {useEffect, useMemo} from "react";
 import {fetchVenues} from "../../store/modules/venueSlice";
 import {useState} from "react";
 import {MagnifyingGlassIcon} from "@heroicons/react/20/solid";
@@ -11,7 +11,9 @@ function Home()   {
     const [search, setSearch] = useState();
     useEffect(() => {
         dispatch(fetchVenues());
-    }, [dispatch])
+    }, [dispatch]);
+    const memoVenue = useMemo(() => venues, [venues])
+
     return (
         <>
             <div className="bg-white">
@@ -28,7 +30,7 @@ function Home()   {
                             <input
                                 id="search"
                                 name="search"
-                                //onChange={(e) => setSearch(e.target.value)}
+                                onChange={(e) => setSearch(e.target.value)}
                                 className="block w-full rounded-md border-0 bg-white py-1.5 pl-10 pr-3 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 mx-auto"
                                 placeholder="Search for venues"
                                 type="search"
@@ -36,7 +38,7 @@ function Home()   {
                         </div>
                     </div>
                     <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-                        {venues.map((venue) => (
+                        {memoVenue.map((venue) => (
                             <div key={venue.id} className="group">
                                 <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-50 xl:aspect-h-8 xl:aspect-w-7">
                                     <img
@@ -46,7 +48,7 @@ function Home()   {
                                     />
                                 </div>
                                 <h3 className="mt-4 text-sm text-gray-700">{venue.name}</h3>
-                                <h3 className=" text-sm text-gray-700">Price: {venue.price}</h3>
+                                <h3 className=" text-sm text-gray-700">Price: {venue.price}$</h3>
                                 <h3 className="text-sm text-gray-700">Rating: {venue.rating}/10</h3>
                                 <Link to={`venueDetail/${venue.id}`} className="rounded-md  px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm bg-indigo-600 hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
                                     <button className="mt-4">View Venue</button>
