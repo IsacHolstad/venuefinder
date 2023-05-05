263a60551cf532e79aa34a718d1a746926053acf
IsacHolstad <wisac22@gmail.com>
2023-05-05 10:11:51 +0200

    create venue form done

--- src/components/views/CreateVenue.js
+++ src/components/views/CreateVenue.js
@@ -2,10 +2,93 @@ import React from 'react';
 
 const CreateVenue = () => {
     return (
-        <div>
-            <h2>CREATE VENUE PAGE</h2>
+        <>
+            <div className="flex min-h-full flex-1 flex-col justify-center py-12 sm:px-6 lg:px-8">
+                <div className="sm:mx-auto sm:w-full sm:max-w-md">
+                    <h2 className="mt-6 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+                        Create Venue
+                    </h2>
+                </div>
+                <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-[480px]">
+                    <div className="bg-white px-6 py-12 shadow sm:rounded-lg sm:px-12">
+                        <form  className="space-y-6" action="#" method="POST">
+                            <div>
+                                <label htmlFor="name" className="block text-sm font-medium leading-6 text-gray-900">
+                                    Name
+                                </label>
+                                <div className="mt-2">
+                                    <input
+                                        id="name"
+                                        name="name"
+                                        type="text"
+                                        autoComplete="email"
+                                        required
+                                        className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                    />
+                                </div>
+                            </div>
+                            <div>
+                                <label htmlFor="location" className="block text-sm font-medium leading-6 text-gray-900">
+                                    Location
+                                </label>
+                                <div className="mt-2">
+                                    <input
+                                        id="location"
+                                        name="location"
+                                        type="text"
+                                        required
+                                        className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                    />
+                                </div>
+                            </div>
+                            <div>
+                                <label htmlFor="image" className="block text-sm font-medium leading-6 text-gray-900">
+                                    Image
+                                </label>
+                                <div className="mt-2">
+                                    <input
+                                        id="image"
+                                        name="image"
+                                        type="text"
+                                        placeholder="Image URL"
+                                        className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                    />
+                                </div>
+                            </div>
 
-        </div>
+                            <div>
+                                <label htmlFor="description" className="block text-sm font-medium leading-6 text-gray-900">
+                                    Description
+                                </label>
+                                <div className="mt-2">
+                                    <textarea
+                                        id="description"
+                                        name="description"
+                                        required
+                                        placeholder="Write description of youre venue"
+                                        className="block w-full h-32 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                    />
+                                </div>
+                            </div>
+                            <div className="flex items-center justify-between">
+                            </div>
+                            <div>
+                                <button
+                                    type="submit"
+                                    className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                                >
+                                    Submit
+                                </button>
+                            </div>
+                        </form>
+                        <div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+        </>
     );
 };
 
