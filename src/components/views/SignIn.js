10a30d49e78e37d395ea812a647f9465cba8b159
Hesham <heshamelmasry1992@gmail.com>
2023-05-15 13:08:05 +0200

    for changes

--- src/components/views/SignIn.js
+++ src/components/views/SignIn.js
@@ -4,27 +4,25 @@ import React, {useEffect} from 'react';
 import {useState} from "react";
 
 const SignIn = () => {
-    const [setEmail, email] = useState("");
-    const [setPassword, password] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
 
-   async function loggingIn(){
+    async function loggingIn(event) {
+        event.preventDefault();
 
         console.log(password, email)
         let item = {email, password};
-       console.log(item)
-       let result =  await fetch('https://nf-api.onrender.com/api/v1/holidaze/auth/login', {
-           method: 'POST',
-           headers: {
-               "Content-Type" : "application/json",
-               "Accept": 'application/json'
-           },
-           body: JSON.stringify(item)
-       });
-        result = await result.json();
-        localStorage.setItem("user-info", JSON.stringify(result))
-
+        console.log(item)
+        // let result =  await fetch('https://nf-api.onrender.com/api/v1/holidaze/auth/login', {
+        //     method: 'POST',
+        //     headers: {
+        //         "Content-Type" : "application/json"
+        //     },
+        //     body: item
+        // });
+        //  result = await result.json();
+        //  localStorage.setItem("user-info", JSON.stringify(result))
     }
-
 
 
     return (
@@ -37,7 +35,7 @@ const SignIn = () => {
                 </div>
                 <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-[480px]">
                     <div className="bg-white px-6 py-12 shadow sm:rounded-lg sm:px-12">
-                        <form className="space-y-6">
+                        <form className="space-y-6" onSubmit={loggingIn}>
                             <div>
                                 <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
                                     Email
@@ -72,13 +70,11 @@ const SignIn = () => {
                             <div className="flex items-center justify-between">
                             </div>
                             <div>
-                                <button
+                                <input
                                     type="submit"
-                                    onClick={loggingIn}
+                                    value="Sign in"
                                     className="flex w-full justify-center rounded-md  px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm bg-indigo-600 hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                                >
-                                    Sign in
-                                </button>
+                                />
                             </div>
                         </form>
                         <div>
